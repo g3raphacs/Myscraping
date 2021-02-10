dfd7212fbf52cb49fcac6a895c666692e9a3392b
g3raphacs <g3raphacs@gmail.com>
2021-02-10 19:00:56 +0100

    header template

--- public/index.php
+++ public/index.php
@@ -2,55 +2,63 @@
 session_start();
 
 require '../vendor/autoload.php';
+use App\Router\Router;
+use App\Twig\Twig;
+use App\Session\SessionManager;
 
-$router = new App\Router\Router($_GET['url']);
+$router = new Router($_GET['url']);
 
+//root
 $router->get('/', function(){
-    $twig = new App\Twig\Twig('logo.html.twig');
+    $twig = new Twig('logo.html.twig');
     $twig->render();
 
-    $session = new \App\Session\SessionManager;
+    $session = new SessionManager;
     if($session->checkSession()){
         header('Location: ./'.$_SESSION['scraplist']);
     }
 });
 
+//signup
 $router->get('/inscription', function(){
-    $session = new \App\Session\SessionManager;
+    $session = new SessionManager;
     $session->killSession();
 
-    $twig = new App\Twig\Twig('inscription.html.twig');
+    $twig = new Twig('inscription.html.twig');
     $twig->render();
-
 });
 
+//signin
 $router->get('/connexion', function(){
-    $session = new \App\Session\SessionManager;
+    $session = new SessionManager;
     $session->killSession();
 
-    $twig = new App\Twig\Twig('connexion.html.twig');
+    $twig = new Twig('connexion.html.twig');
     $twig->render();
-
 });
 
+//dashboard
 $router->get('/:userslug', function($userslug){
     
-    $session = new \App\Session\SessionManager;
+    $session = new SessionManager;
     if(!$session->checkSession()){
         $session->killSession();
         header('Location: ./');
     }
-    echo "page de l'utilisateur : " . $userslug;
+
+    $twig = new Twig('dashboard.html.twig');
+    $twig->render();
 });
 
 
 
 //AJAX ROUTES
+//login
 $router->post('login', function(){
     $userM = new \App\User\UserManager;
     $userM->signin();
 });
-
+//signup
 $router->post('signup', function(){
     $userM = new \App\User\UserManager;
     $userM->signup();
